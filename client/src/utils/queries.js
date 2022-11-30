030c02c5ed8b5876b0e9b93688f897a7dd2ab114
bordanattila <bordan_attila@hotmail.com>
2022-11-30 16:21:43 -0500

    add schemas folder with index.js, resolver.js and typeDef.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+query user($username: String!) {
+    user(username: $username) {
+        _id
+        username
+        email
+        savedBooks
+        bookCount
+    }
+}`
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,73 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { Book, User } = require("../models");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id });
+        }
+        throw new AuthenticationError("You need to be logged in!");
+      },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+        // Look up the user by the provided email address. Since the `email` field is unique, only one person will exist with that email
+        const user = await User.findOne({ email });
+  
+        // If there is no user with that email address, return an Authentication error 
+        if (!user) {
+          throw new AuthenticationError("No user found with this email address");
+        }
+  
+        // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+        const correctPassword = await user.isCorrectPassword(password);
+  
+        // If the password is incorrect, return an Authentication error 
+        if (!correctPassword) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+  
+        // If email and password are correct, sign user into the application with a JWT
+        const token = signToken(user);
+  
+        // Return an `Auth` object that consists of the signed token and user"s information
+        return { token, user };
+      },
+      addUser: async (parent, { username, email, password }) => {
+        // Create the user
+        const user = await User.create({ username, email, password });
+        // Sign a JSON Web Token and log the user in after they are created
+        const token = signToken(user);
+        // Return an `Auth` object that consists of the signed token and user"s information
+        return { token, user };
+      },
+      saveBook: async (parent, { authors, description, title, bookId, image, link }) => {
+        const book = await Book.create({ authors, description, title, bookId, image, link });
+  
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: book._id } }
+        );
+  
+        return book;
+      },
+      removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+            const book = await Book.findOneAndDelete({
+                _id: bookId,
+            });
+
+            await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: book._id}}
+            );
+
+            return book;
+        }
+        throw new AuthenticationError("You need to be logged in!");
+      }
+  },
+};
+
+module.exports = resolvers;
